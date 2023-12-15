6fcdcfe79be027132b3c2af46f636db3d06d2812
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-15 19:03:23 +0530

    birthday buddy project ccomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState } from 'react';
+import data from './data';
+import Person from './Person';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [userdata, setUserData] = useState(data);
+
+  const handleClick = () => {
+    setUserData([]);
+  };
+
+  return (
+    <main>
+      <div className="container">
+        <h3>{userdata.length} birthdays today</h3>
+        <ul>
+          {userdata.map((user) => {
+            return <Person user={user} key={user.id} />;
+          })}
+        </ul>
+        <button className="btn" onClick={handleClick}>
+          clear All
+        </button>
+      </div>
+    </main>
+  );
 };
 export default App;
---
+++ src/Person.jsx
@@ -0,0 +1,22 @@
+const Person = (props) => {
+  const { name, image, age } = props.user;
+  //   const handleClick = () => {
+  //     console.log('i got clicked');
+  //   };
+  return (
+    <li className="person">
+      <img src={image} alt={name} />
+      <div className="person-info">
+        <h4>{name}</h4>
+        <p>{age} years</p>
+        {/* <span>
+          <button className="btn-cancel" onClick={handleClick}>
+            x
+          </button>
+        </span>
+        <hr /> */}
+      </div>
+    </li>
+  );
+};
+export default Person;
